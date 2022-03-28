ab7c4f94b47dc0c993fe6c377019b568217802b2
Miles <Miles@DESKTOP-47FK9RH>
2022-03-29 00:13:10 +0200

    hem fer una proba

--- Activitat10Git/src/activitat10git/STRINGMARAVILLOSO.java
+++ Activitat10Git/src/activitat10git/STRINGMARAVILLOSO.java
@@ -2,16 +2,22 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+*@author Miles
  */
 package activitat10git;
+
+import static imprimir.Imprimir.imprimirInferiorTriangle;
+import static imprimir.Imprimir.imprimirSuperiorTriangle;
+
 public class STRINGMARAVILLOSO{
     public static void main(String[] args){
         String M = "MARAVILLOSO";
         
-        System.out.println("LA versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
+        System.out.println("La versió que tenim a github ha estat actualitzada i és mes actual que la que tinc en local");
  
         //Imprime los primeros dos triangulos de izq a derecha de la primera parte (parte superios)
-        for (int i =0; i<M.length();i++){
+        imprimirSuperiorTriangle(M);
+        /**for (int i =0; i<M.length();i++){
             //Imprime espacios de la primera parte para el primer triangulo 
             for (int j=0; j < i  ;++j){
                 System.out.print(" ");
@@ -29,8 +35,9 @@ public class STRINGMARAVILLOSO{
                 System.out.print(M.charAt(j));
             }
             System.out.println();
-        }
-        //Imprime los segundos dos triangulos de izq a derecha de la segunda parte (parte inferior)
+        }*/
+        imprimirInferiorTriangle(M);
+        /**Imprime los segundos dos triangulos de izq a derecha de la segunda parte (parte inferior)
         for (int i =0; i<M.length();i++){
             //Imprime primer triangulo de la segunda parte
             for (int j= i; j<M.length(); ++j){
@@ -45,6 +52,6 @@ public class STRINGMARAVILLOSO{
                 System.out.print(M.charAt(j));
             }
             System.out.println();
-        }
+        }*/
     }    
 }
